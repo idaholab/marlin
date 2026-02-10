idaholab/swift

Add old state (#50)

src/tensor_computes/NEML2TensorCompute.C
@@ -67,13 +67,55 @@ NEML2TensorCompute::NEML2TensorCompute(const InputParameters & params)
   NEML2Utils::assertNEML2Enabled();
 
 #ifdef NEML2_ENABLED
-  for (const auto & [swift_input_name, neml2_input_name] :
-       getParam<TensorInputBufferName, std::string>("swift_inputs", "neml2_inputs"))
+  const auto inputs = getParam<TensorInputBufferName, std::string>("swift_inputs", "neml2_inputs");
+  std::map<neml2::LabeledAxisAccessor, TensorInputBufferName> lookup_swift_name;
+  const auto model_inputs = _model.consumed_items();
+
+  // current inputs
+  for (const auto & [swift_input_name, neml2_input_name] : inputs)
   {
-    const auto * input_buffer = &getInputBufferByName<>(swift_input_name);
-    _input_mapping.emplace_back(
-        input_buffer, neml2::LabeledAxisAccessor(NEML2Utils::parseVariableName(neml2_input_name)));
+    const auto neml2_input =
+        neml2::LabeledAxisAccessor(NEML2Utils::parseVariableName(neml2_input_name));
+
+    // populate reverse lookup map
+    if (lookup_swift_name.find(neml2_input) != lookup_swift_name.end())
+      mooseError("Repeated NEML2 input ", neml2_input_name);
+    lookup_swift_name[neml2_input] = swift_input_name;
+
+    // the user should only specify current neml2 axis
+    if (!neml2_input.is_state() && !neml2_input.is_force())
+      mooseError("Specify only current forces or states as inputs. Old forces and states are "
+                 "automatically coupled when needed.");
+
+    // add input if the model requires it
+    if (model_inputs.count(neml2_input))
+    {
+      const auto * input_buffer = &getInputBufferByName<>(swift_input_name);
+      const auto type = _model.input_variable(neml2_input).type();
+      _input_mapping.emplace_back(input_buffer, type, neml2_input);
+    }
   }
+
+  // old state inputs
+  for (const auto & neml2_input : model_inputs)
+    if (neml2_input.is_old_state())
+    {
+      // check if we couple the current state
+      auto it = lookup_swift_name.find(neml2_input.current());
+      if (it == lookup_swift_name.end())
+        mooseError("The model requires ",
+                   neml2_input,
+                   " but no tensor buffer is assigned to ",
+                   neml2_input.current(),
+                   ".");
+      const auto & swift_input_name = it->second;
+
+      const auto * old_states = &getBufferOldByName<>(swift_input_name, 1);
+      // we also get the current state here just to step zero, when no old state exists!
+      const auto * input_buffer = &getInputBufferByName<>(swift_input_name);
+      const auto type = _model.input_variable(neml2_input).type();
+      _old_input_mapping.emplace_back(old_states, input_buffer, type, neml2_input);
+    }
 
   for (const auto & [neml2_output_name, swift_output_name] :
        getParam<std::string, TensorInputBufferName>("neml2_outputs", "swift_outputs"))
@@ -99,20 +141,30 @@ NEML2TensorCompute::computeBuffer()
 {
 #ifdef NEML2_ENABLED
   neml2::ValueMap in;
-  for (const auto & [tensor_ptr, label] : _input_mapping)
+  auto insert_tensor = [&in, this](const auto & tensor, auto type, const auto & label)
   {
     // convert tensors on the fly at runtime
-    auto sizes = tensor_ptr->sizes();
-    mooseInfoRepeated(name(), " sizes size ", sizes.size(), " is ", Moose::stringify(sizes));
-    if (sizes.size() == _dim)
-      in[label] = neml2::Scalar(*tensor_ptr);
-    else if (sizes.size() == _dim + 1)
-      in[label] = neml2::Vec(*tensor_ptr, _domain.getShape());
-    else if (sizes.size() == _dim + 3)
-      in[label] = neml2::R2(*tensor_ptr, _domain.getShape());
+    auto sizes = tensor.sizes();
+    if (sizes.size() == _dim && type == neml2::TensorType::kScalar)
+      in[label] = neml2::Scalar(tensor);
+    else if (sizes.size() == _dim + 1 && type == neml2::TensorType::kVec)
+      in[label] = neml2::Vec(tensor, _domain.getShape());
+    else if (sizes.size() == _dim + 3 && type == neml2::TensorType::kR2)
+      in[label] = neml2::R2(tensor, _domain.getShape());
     else
-      mooseError("Unsupported tensor dimension");
-  }
+      mooseError("Unsupported/mismatching tensor dimension");
+  };
+
+  // insert current state
+  for (const auto & [current_state, type, label] : _input_mapping)
+    insert_tensor(*current_state, type, label);
+
+  // insert old state
+  for (const auto & [old_states, current_state, type, label] : _old_input_mapping)
+    if (old_states->empty())
+      insert_tensor(*current_state, type, label);
+    else
+      insert_tensor((*old_states)[0], type, label);
 
   auto out = _model.value(in);
 

idaholab/marlin

src/tensor_computes/NEML2TensorCompute.C
@@ -66,13 +66,55 @@ NEML2TensorCompute::NEML2TensorCompute(const InputParameters & params)
   NEML2Utils::assertNEML2Enabled();
 
 #ifdef NEML2_ENABLED
-  for (const auto & [marlin_input_name, neml2_input_name] :
-       getParam<TensorInputBufferName, std::string>("marlin_inputs", "neml2_inputs"))
+  const auto inputs = getParam<TensorInputBufferName, std::string>("marlin_inputs", "neml2_inputs");
+  std::map<neml2::LabeledAxisAccessor, TensorInputBufferName> lookup_marlin_name;
+  const auto model_inputs = _model.consumed_items();
+
+  // current inputs
+  for (const auto & [marlin_input_name, neml2_input_name] : inputs)
   {
-    const auto * input_buffer = &getInputBufferByName<>(marlin_input_name);
-    _input_mapping.emplace_back(
-        input_buffer, neml2::LabeledAxisAccessor(NEML2Utils::parseVariableName(neml2_input_name)));
+    const auto neml2_input =
+        neml2::LabeledAxisAccessor(NEML2Utils::parseVariableName(neml2_input_name));
+
+    // populate reverse lookup map
+    if (lookup_marlin_name.find(neml2_input) != lookup_marlin_name.end())
+      mooseError("Repeated NEML2 input ", neml2_input_name);
+    lookup_marlin_name[neml2_input] = marlin_input_name;
+
+    // the user should only specify current neml2 axis
+    if (!neml2_input.is_state() && !neml2_input.is_force())
+      mooseError("Specify only current forces or states as inputs. Old forces and states are "
+                 "automatically coupled when needed.");
+
+    // add input if the model requires it
+    if (model_inputs.count(neml2_input))
+    {
+      const auto * input_buffer = &getInputBufferByName<>(marlin_input_name);
+      const auto type = _model.input_variable(neml2_input).type();
+      _input_mapping.emplace_back(input_buffer, type, neml2_input);
+    }
   }
+
+  // old state inputs
+  for (const auto & neml2_input : model_inputs)
+    if (neml2_input.is_old_state())
+    {
+      // check if we couple the current state
+      auto it = lookup_marlin_name.find(neml2_input.current());
+      if (it == lookup_marlin_name.end())
+        mooseError("The model requires ",
+                   neml2_input,
+                   " but no tensor buffer is assigned to ",
+                   neml2_input.current(),
+                   ".");
+      const auto & marlin_input_name = it->second;
+
+      const auto * old_states = &getBufferOldByName<>(marlin_input_name, 1);
+      // we also get the current state here just to step zero, when no old state exists!
+      const auto * input_buffer = &getInputBufferByName<>(marlin_input_name);
+      const auto type = _model.input_variable(neml2_input).type();
+      _old_input_mapping.emplace_back(old_states, input_buffer, type, neml2_input);
+    }
 
   for (const auto & [neml2_output_name, marlin_output_name] :
        getParam<std::string, TensorInputBufferName>("neml2_outputs", "marlin_outputs"))
@@ -98,19 +140,30 @@ NEML2TensorCompute::computeBuffer()
 {
 #ifdef NEML2_ENABLED
   neml2::ValueMap in;
-  for (const auto & [tensor_ptr, label] : _input_mapping)
+  auto insert_tensor = [&in, this](const auto & tensor, auto type, const auto & label)
   {
     // convert tensors on the fly at runtime
-    auto sizes = tensor_ptr->sizes();
-    if (sizes.size() == _dim)
-      in[label] = neml2::Scalar(*tensor_ptr);
-    else if (sizes.size() == _dim + 1)
-      in[label] = neml2::Vec(*tensor_ptr, _domain.getShape());
-    else if (sizes.size() == _dim + 3)
-      in[label] = neml2::R2(*tensor_ptr, _domain.getShape());
+    auto sizes = tensor.sizes();
+    if (sizes.size() == _dim && type == neml2::TensorType::kScalar)
+      in[label] = neml2::Scalar(tensor);
+    else if (sizes.size() == _dim + 1 && type == neml2::TensorType::kVec)
+      in[label] = neml2::Vec(tensor, _domain.getShape());
+    else if (sizes.size() == _dim + 3 && type == neml2::TensorType::kR2)
+      in[label] = neml2::R2(tensor, _domain.getShape());
     else
-      mooseError("Unsupported tensor dimension");
-  }
+      mooseError("Unsupported/mismatching tensor dimension");
+  };
+
+  // insert current state
+  for (const auto & [current_state, type, label] : _input_mapping)
+    insert_tensor(*current_state, type, label);
+
+  // insert old state
+  for (const auto & [old_states, current_state, type, label] : _old_input_mapping)
+    if (old_states->empty())
+      insert_tensor(*current_state, type, label);
+    else
+      insert_tensor((*old_states)[0], type, label);
 
   auto out = _model->value(in);
 